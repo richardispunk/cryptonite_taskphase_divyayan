87bec45fce82be65d13ca7c679b5b7014839339a
richardispunk <divyayan.d@gmail.com>
2025-01-05 11:30:07 +0530

    Update temp_change.py

--- temp_change.py
+++ temp_change.py
@@ -2,4 +2,4 @@
 even = [23, 26, 25, 14, 7, 29, 21, 33, 1, 2, 3, 15, 99, 101, 7, 505, 69, 420, 80085, 30, 56, 18, 10]
 ls = [digit for digit in [num for num in even if num%2 == 0] if digit in range(51)]
 for i in ls:
-  print(ls, '\n', sep='')
+  print(i, '\n', sep='')
